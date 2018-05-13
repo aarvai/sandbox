68ec06e5c8d93b0b1684953383e38aeadd467cbb
Amanda Arvai <aarvai@stsci.edu>
2018-05-13 12:09:52 -0400

    Added ttk themes.

--- temp.py
+++ temp.py
@@ -7,30 +7,46 @@ Created on Sat May 12 13:08:54 2018
 
 from tkinter import *
 from tkinter import Button, Entry
+from tkinter import ttk
+
+#Select tkinter style
+#s = ttk.Style()
+#print("Tkinter theme options are:")
+#print(s.theme_names())
+#print("Current Tkinter theme:")
+#print(s.theme_use())
+#s.theme_use('clam')
 
 
-class App:
+class AcsGui:
 
     enter_value: Entry
 
     def __init__(self, master):
+
         global v
         v = StringVar()
 
-        frame = Frame(master)
+        print("Tkinter theme options are:")
+        print(ttk.Style().theme_names())
+        print("Current Tkinter theme:")
+        print(ttk.Style().theme_use())
+        ttk.Style().theme_use('vista')
+
+        frame = ttk.Frame(master)
         frame.pack()
 
-        self.enter_value = Entry(frame, width=50, textvariable=v)
+        self.enter_value = ttk.Entry(frame, width=50, textvariable=v)
         self.enter_value.pack(side=LEFT)
         self.enter_value.focus_set()
 
-        self.button1 = Button(frame, text="Print Value", fg="red", command=self.print_value)
+        self.button1 = ttk.Button(frame, text="Print Value", command=self.print_value)
         self.button1.pack(side=LEFT)
 
-        self.button2 = Button(frame, text="Gray it out", fg="red", command=self.gray_it_out)
+        self.button2 = ttk.Button(frame, text="Gray it out", command=self.gray_it_out)
         self.button2.pack(side=LEFT)
 
-        self.button3 = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button3 = ttk.Button(frame, text="QUIT", command=frame.quit)
         self.button3.pack(side=LEFT)
 
     def print_value(self):
@@ -44,6 +60,6 @@ class App:
 
 root = Tk()
 
-app = App(root)
+app = AcsGui(root)
 
 root.mainloop()
