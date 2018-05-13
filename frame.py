8e3f2c6d1836aed8475f4b938e0898b10e077cf5
Amanda Arvai <aarvai@stsci.edu>
2018-05-12 20:02:08 -0400

    Syntax fixes.

--- frame.py
+++ frame.py
@@ -1,19 +1,21 @@
 from tkinter import *
 
-class App
+class App:
 
-    def __init__(self, master)
+    def __init__(self, master):
 
         frame = Frame(master)
         frame.pack()
 
-        self.button = Button(frame, text=QUIT, fg=red, command=frame.quit)
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
         self.button.pack(side=LEFT)
 
-        self.hi_there = Button(frame, text=Hello, command=self.say_hi)
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
         self.hi_there.pack(side=LEFT)
 
-    def say_hi(self)
+    def say_hi(self):
         print("hi there, everyone!")
 
 root = Tk()
