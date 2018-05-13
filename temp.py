5bf90af6a0e101c99bb482c209a7d696c815da94
Amanda Arvai <aarvai@stsci.edu>
2018-05-12 22:22:36 -0400

    Syntax fixes.

--- temp.py
+++ temp.py
@@ -6,8 +6,12 @@ Created on Sat May 12 13:08:54 2018
 """
 
 from tkinter import *
+from tkinter import Button, Entry
+
 
 class App:
+
+    enter_value: Entry
 
     def __init__(self, master):
         global v
@@ -20,14 +24,24 @@ class App:
         self.enter_value.pack(side=LEFT)
         self.enter_value.focus_set()
 
-        self.button = Button(frame, text="Print Value", fg="red", command=self.print_value)
-        self.button.pack(side=LEFT)
-		
+        self.button1 = Button(frame, text="Print Value", fg="red", command=self.print_value)
+        self.button1.pack(side=LEFT)
+
+        self.button2 = Button(frame, text="Gray it out", fg="red", command=self.gray_it_out)
+        self.button2.pack(side=LEFT)
+
+        self.button3 = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button3.pack(side=LEFT)
+
     def print_value(self):
         print("hi there")
         #print(self.enter_value.get())
         print(v.get())
-		
+
+    def gray_it_out(self):
+        self.enter_value.config(state=DISABLED)
+
+
 root = Tk()
 
 app = App(root)
